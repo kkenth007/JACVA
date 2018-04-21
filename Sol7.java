5e6692a8482cf9ea92857c24fd6811d017beec1a
Kanit Wijitpanya <32562129+kkenth007@users.noreply.github.com>
2018-04-21 15:24:12 +0700

    Green white

---
+++ Sol7.java
@@ -0,0 +1,11 @@
+package sol7;
+public class Sol7 {
+    public static void main(String[] args) {
+        Sol7 myName = new Sol7();
+        myName.hello();
+    }
+    public void hello(){
+        for(int i=1;i<=10;i++)
+            System.out.println("Kanit Wijitpanya");
+    }
+}
