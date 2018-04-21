4203b5607ba9b5adb61ec5a288a5f6fcae7e5066
Kanit Wijitpanya <32562129+kkenth007@users.noreply.github.com>
2018-04-21 15:25:40 +0700

    sol9

---
+++ Sol9.java
@@ -0,0 +1,13 @@
+package sol9;
+public class Sol9 {
+    
+    public static void main(String[] args) {
+        double areaOne = cal.area(38);
+        double areatwo = cal.area(16,8);
+        double areathree= cal.area(12, 8);
+        System.out.println("พื้นที่รูปสี่เหลี่ยม รูปว่าว : "+areaOne);
+        System.out.println("พื้นที่รูปสี่เหลี่ยม คางหมู : "+areatwo);
+        System.out.println("พื้นที่รูปสี่เหลี่ยม ด้านไม่เท่า : "+areathree);
+    }
+
+}
---
+++ cal.java
@@ -0,0 +1,16 @@
+package sol9;
+public final class cal {
+    
+    public static double area(int sqr2Diagonal){
+    return (0.5*sqr2Diagonal);//ผลคูณเส้นทแยงมุม
+    }
+    
+    public static double area(int toplus,int high){
+    return (0.5*toplus*high);//ผลบวกด้านคู่ขนาน กับ ความสูง
+    }
+    
+    public static double area(double Diagonal,double left){
+    return (0.5*Diagonal*left);//เส้นทแยงมุม กับ ผลบวกเส้นกิ่ง
+    }
+}
+
